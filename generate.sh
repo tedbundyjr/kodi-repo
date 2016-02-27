cd829978e96c5217d07b3517fbdb484eb3019e28
Haris Subandie Md Suhaimin <haris@vms.my>
2016-02-27 23:07:49 +0800

    fixing parser issue

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # rm -rf _repo
-rsync -av --exclude=repository.vms --exclude=repository.bluecop.xbmc-plugins --exclude=repository.BlazeRepo ~/Library/Application\ Support/Kodi/addons/ .
+rsync -av --exclude=repository.boogie --exclude=repository.vms ~/Library/Application\ Support/Kodi/addons/ .
 cd _tools 
 python generate_repo.py
 cd -
