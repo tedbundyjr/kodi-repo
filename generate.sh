f6052eeaba4a58699b6c90b14cd5b8ec47a24b29
Haris Subandie Md Suhaimin <haris@vms.my>
2016-02-29 12:03:45 +0800

    ignore .git

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # rm -rf _repo
-rsync -av --exclude=packages --exclude=plugin.video.SportsDevil --exclude=repository.boogie --exclude=repository.vms ~/Library/Application\ Support/Kodi/addons/ .
+rsync -av --exclude=.git --exclude=packages --exclude=plugin.video.SportsDevil --exclude=repository.boogie --exclude=repository.vms ~/Library/Application\ Support/Kodi/addons/ .
 cd _tools 
 python generate_repo.py
 cd -
