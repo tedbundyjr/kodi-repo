9f60659e438f99903893f4414fd0032c85322064
Haris Subandie Md Suhaimin <haris@vms.my>
2016-03-04 00:11:08 +0800

    test webhook

--- webhook.php
+++ webhook.php
@@ -1,12 +1,15 @@
 <?php
-set_time_limit(300); // 5mins
-// test 3
+
+$json = $_POST['payload'];
+
+var_dump(json_decode($json));
+
+die();
 /**
  * GitHub webhook handler template.
  *
  * @see  https://developer.github.com/webhooks/
  */
- 
 $hookSecret = ''; # set NULL to disable check
 set_error_handler(function ($severity, $message, $file, $line) {
 	throw new \ErrorException($message, 0, $severity, $file, $line);
@@ -19,26 +22,26 @@ set_exception_handler(function ($e) {
 
 $rawPost = null;
 if ($hookSecret !== null) {
-	// if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
-	// 	throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
-	// } elseif (!extension_loaded('hash')) {
-	// 	throw new \Exception("Missing 'hash' extension to check the secret code validity.");
-	// }
-	// list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
-	// if (!in_array($algo, hash_algos(), true)) {
-	// 	throw new \Exception("Hash algorithm '$algo' is not supported.");
-	// }
-	// $rawPost = file_get_contents('php://input');
-	// if ($hash !== hash_hmac($algo, $rawPost, $hookSecret)) {
-	// 	throw new \Exception('Hook secret does not match.');
-	// }
+	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+		throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
+	} elseif (!extension_loaded('hash')) {
+		throw new \Exception("Missing 'hash' extension to check the secret code validity.");
+	}
+	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+	if (!in_array($algo, hash_algos(), true)) {
+		throw new \Exception("Hash algorithm '$algo' is not supported.");
+	}
+	$rawPost = file_get_contents('php://input');
+	if ($hash !== hash_hmac($algo, $rawPost, $hookSecret)) {
+		throw new \Exception('Hook secret does not match.');
+	}
 }
 
-// if (!isset($_SERVER['HTTP_CONTENT_TYPE'])) {
-// 	throw new \Exception("Missing HTTP 'Content-Type' header.");
-// } elseif (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
-// 	throw new \Exception("Missing HTTP 'X-Github-Event' header.");
-// }
+if (!isset($_SERVER['HTTP_CONTENT_TYPE'])) {
+	throw new \Exception("Missing HTTP 'Content-Type' header.");
+} elseif (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
+	throw new \Exception("Missing HTTP 'X-Github-Event' header.");
+}
 
 switch ($_SERVER['HTTP_CONTENT_TYPE']) {
 case 'application/json':
@@ -54,15 +57,13 @@ default:
 # Payload structure depends on triggered event
 # https://developer.github.com/v3/activity/events/types/
 $payload = json_decode($json);
-
 switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
 
 case 'ping':
 	echo 'pong';
 	break;
-case 'push':
-	passthru("/usr/bin/git pull origin master");
-        break;
+//    case 'push':
+//        break;
 //    case 'create':
 //        break;
 default:
