069678107b3de35d86f8b473fcb62fe435a745fe
Haris Subandie Md Suhaimin <haris@vms.my>
2016-03-22 12:25:47 +0800

    update script + dramago

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # rm -rf _repo
+./get.sh
 rsync -av --exclude=.git --exclude=packages --exclude=plugin.video.SportsDevil --exclude=repository.boogie --exclude=repository.vms ~/Library/Application\ Support/Kodi/addons/ .
 cd _tools 
 python generate_repo.py
