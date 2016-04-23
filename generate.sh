109fcf0aff544d741a03a540f19e05daf1965310
Haris Subandie Md Suhaimin <haris@vms.my>
2016-04-23 09:52:36 +0800

    update sportsdevil

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # rm -rf _repo
 ./get.sh
-rsync -av --exclude=.git --exclude=packages --exclude=plugin.video.SportsDevil --exclude=repository.boogie --exclude=repository.vms ~/Library/Application\ Support/Kodi/addons/ .
+rsync -av --exclude=.git --exclude=packages --exclude=repository.boogie --exclude=repository.vms ~/Library/Application\ Support/Kodi/addons/ .
 cd _tools 
 python generate_repo.py
 cd -
